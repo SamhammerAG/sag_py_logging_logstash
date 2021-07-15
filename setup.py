bba8ad4b609ec8bac27a8f6f9adf401f3a8afc61
oschulz <oschulz@samhammer.de>
2021-07-15 15:10:33 +0200

    KIT-1033 increased version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from shutil import rmtree
 import sys
 
 NAME = 'python-logstash-async'
-VERSION = '2.3.2'
+VERSION = '2.3.3'
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), 'rb') as f:
