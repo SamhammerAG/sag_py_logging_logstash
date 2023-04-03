a503104d74dcda76a0c63ec8df30d94dc2c7e079
Stefan Rom <srom@samhammer.de>
2023-04-03 17:26:12 +0200

    Prepare for releasing 2.3.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fin:
 
 setuptools.setup(
     name="sag-py-logging-logstash",
-    version="0.1.0",
+    version="2.3.4",
     description="Python logging logstash handler",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
