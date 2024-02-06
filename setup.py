d407d4eb1c86c986fd232f9e7d27fa736ef399d5
Farzali Absalamov <fabsalamov@samhammer.de>
2024-02-06 11:59:49 +0000

    KIT-3207 updated devcontainer and settings files.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements-dev.txt", "r") as fin:
 
 setuptools.setup(
     name="sag-py-logging-logstash",
-    version="2.3.6",
+    version="2.3.7",
     description="Python logging logstash handler",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
