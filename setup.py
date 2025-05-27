cdec6aed1af24a272396fc97604e5c1077376bff
aott <ottalexanderdev@gmail.com>
2025-05-27 13:52:03 +0200

    Bump package version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements-dev.txt", "r") as fin:
 
 setuptools.setup(
     name="sag-py-logging-logstash",
-    version="3.0.1",
+    version="3.0.2",
     description="Python logging logstash handler",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
