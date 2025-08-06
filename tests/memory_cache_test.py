f7e1b25885a045aee050c0a39886c3855bbe7519
Stefan Rom <41320207+devRoemer@users.noreply.github.com>
2025-08-06 16:10:19 +0200

    KIT-4207 Adjust unit tests

--- tests/memory_cache_test.py
+++ tests/memory_cache_test.py
@@ -8,6 +8,7 @@ import unittest
 
 from sag_py_logging_logstash.constants import constants
 from sag_py_logging_logstash.memory_cache import MemoryCache
+from sag_py_logging_logstash.safe_logger import SafeLogger
 
 # pylint: disable=protected-access
 
@@ -15,7 +16,8 @@ from sag_py_logging_logstash.memory_cache import MemoryCache
 class MemoryCacheTest(unittest.TestCase):
     # ----------------------------------------------------------------------
     def test_add_event(self):
-        cache = MemoryCache({})
+        safe_logger = SafeLogger()
+        cache = MemoryCache(safe_logger, {})
         cache.add_event("message")
         self.assertEqual(len(cache._cache), 1)
         event = list(cache._cache.values())[0]
@@ -24,14 +26,17 @@ class MemoryCacheTest(unittest.TestCase):
 
     # ----------------------------------------------------------------------
     def test_get_queued_events(self):
-        cache = MemoryCache({"id1": {"pending_delete": True}, "id2": {"pending_delete": False}})
+        safe_logger = SafeLogger()
+        cache = MemoryCache(safe_logger, {"id1": {"pending_delete": True}, "id2": {"pending_delete": False}})
         self.assertEqual(len(cache.get_queued_events()), 1)
 
     # ----------------------------------------------------------------------
     def test_get_queued_events_batch_size(self):
         constants.QUEUED_EVENTS_BATCH_SIZE = 3
+        safe_logger = SafeLogger()
 
         cache = MemoryCache(
+            safe_logger,
             {
                 "id1": {"pending_delete": True},
                 "id2": {"pending_delete": False},
@@ -39,7 +44,7 @@ class MemoryCacheTest(unittest.TestCase):
                 "id4": {"pending_delete": False},
                 "id5": {"pending_delete": False},
                 "id6": {"pending_delete": False},
-            }
+            },
         )
         events = cache.get_queued_events()
         # expect only 3 events according to QUEUED_EVENTS_BATCH_SIZE
@@ -48,37 +53,43 @@ class MemoryCacheTest(unittest.TestCase):
     # ----------------------------------------------------------------------
     def test_get_queued_events_batch_size_underrun(self):
         constants.QUEUED_EVENTS_BATCH_SIZE = 3
+        safe_logger = SafeLogger()
 
-        cache = MemoryCache({"id1": {"pending_delete": True}, "id2": {"pending_delete": False}})
+        cache = MemoryCache(safe_logger, {"id1": {"pending_delete": True}, "id2": {"pending_delete": False}})
         events = cache.get_queued_events()
         # expect only 1 event as there are no more available
         self.assertEqual(len(events), 1)
 
     # ----------------------------------------------------------------------
     def test_get_queued_events_pending_delete_check(self):
-        cache = MemoryCache({"id1": {"pending_delete": False}})
+        safe_logger = SafeLogger()
+        cache = MemoryCache(safe_logger, {"id1": {"pending_delete": False}})
         queued_events = cache.get_queued_events()
         self.assertEqual(len(queued_events), 1)
         self.assertTrue(queued_events[0])
 
     # ----------------------------------------------------------------------
     def test_requeue_queued_events(self):
-        cache = MemoryCache({"id1": {"pending_delete": True}})
+        safe_logger = SafeLogger()
+        cache = MemoryCache(safe_logger, {"id1": {"pending_delete": True}})
         self.assertEqual(len(cache.get_queued_events()), 0)
         cache.requeue_queued_events([{"id": "id1"}])
         self.assertEqual(len(cache.get_queued_events()), 1)
 
     # ----------------------------------------------------------------------
     def test_delete_queued_events(self):
+        safe_logger = SafeLogger()
         cache = MemoryCache(
-            {"id1": {"pending_delete": True, "id": "id1"}, "id2": {"pending_delete": False, "id": "id2"}}
+            safe_logger, {"id1": {"pending_delete": True, "id": "id1"}, "id2": {"pending_delete": False, "id": "id2"}}
         )
         cache.delete_queued_events()
         self.assertEqual(len(cache._cache), 1)
 
     # ----------------------------------------------------------------------
     def test_expire_events(self):
+        safe_logger = SafeLogger()
         cache = MemoryCache(
+            safe_logger,
             {
                 "id1": {"pending_delete": False, "id": "id1", "entry_date": datetime.datetime.fromtimestamp(0)},
                 "id2": {"pending_delete": False, "id": "id2", "entry_date": datetime.datetime.now()},
